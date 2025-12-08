f2999130d4be449c6ad52e79b4aad7b84303ffd5
JairoWarner <91611684+JairoWarner@users.noreply.github.com>
2025-12-08 11:57:45 +0100

    accuracy + HP cap

--- Pocket_monsters.py
+++ Pocket_monsters.py
@@ -1,7 +1,7 @@
 import random
 
 def Bite():
-    return {"name": "Bite", "base_damage": 7}
+    return {"name": "Bite", "base_damage": 7, "accuracy": 90}
 
 def Roll_out():
     return {"name": "Roll_out", "base_damage": 9}
@@ -10,7 +10,7 @@ def Erupt():
     return {"name": "Erupt", "element": "fire", "base_damage": 11}
 
 def Photosyn():
-    return {"name": "Photosyn", "type": "heal", "base_healing": 7}
+    return {"name": "Photosyn", "type": "heal", "base_healing": 7, "accuracy": 90}
 
 def Squirt():
     return {"name": "Squirt", "element": "water", "base_damage": 7}
@@ -57,122 +57,12 @@ def Sing():
 def Tail_Whip():
     return {"name": "Tail_Whip", "base_damage": 7}
 
-turn = False
-
-def Battle_loop(playermns, enemymnst, turn, x_accuracy):
-    battle_hp_player = playermns.HP
-    battle_hp_enemy = enemymnst.HP
-    battle_speed_player = playermns.SPD
-    battle_speed_enemy = enemymnst.SPD
-
-    enemy_sleep_turns = 0
-
-    if battle_speed_player > battle_speed_enemy:
-        turn = True
-    elif battle_speed_enemy > battle_speed_player:
-        turn = False
-    else:
-        turn = random.choice([True, False])
-
-    while battle_hp_player > 0 and battle_hp_enemy > 0:
-        if turn:
-            print("\nYour attacks:")
-            for i, attack in enumerate(playermns.attacks):
-                print(f"{i}: {attack['name']}")
-
-            action = input("\nChoose your attack:")
-
-            if action.isdigit():
-                act_choice = int(action)
-                if act_choice < 0 or act_choice >= len(playermns.attacks):
-                    print("Invalid choice, try again.")
-                    continue
-
-                chosen_attack = playermns.attacks[act_choice]
-                print(f"YOU CHOSE ATTACK {chosen_attack['name']}")
-
-                if chosen_attack.get("type") == "heal":
-                    battle_hp_player += chosen_attack["base_healing"]
-                    print(f"You healed! Your HP is now {battle_hp_player}")
-
-                elif chosen_attack.get("type") == "sleep":
-                    enemy_sleep_turns = random.randint(1, 3)
-                    print(f"The enemy fell asleep for {enemy_sleep_turns} turns!")
-
-                else:
-                    # Default damage
-                    damage = chosen_attack["base_damage"]
-
-                    # Type effectiveness system
-                    if chosen_attack.get("element") == "grass" and enemymnst.type == "water":
-                        damage *= 2
-                        print("It's super effective!")
-                    elif chosen_attack.get("element") == "fire" and enemymnst.type == "grass":
-                        damage *= 2
-                        print("It's super effective!")
-                    elif chosen_attack.get("element") == "water" and enemymnst.type == "fire":
-                        damage *= 2
-                        print("It's super effective!")
-                    elif chosen_attack.get("element") == playermns.type:
-                        damage /= 2
-                        print("It's not very effective...")
-
-                    print(f"Enemy HP before attack: {battle_hp_enemy}")
-                    battle_hp_enemy -= damage
-                    print(f"Enemy HP after attack: {battle_hp_enemy}")
-
-            else:
-                print("Please only enter a number")
-            turn = False
-
-
-        else:
-            if enemy_sleep_turns > 0:
-                print(f"The enemy is fast asleep and can't move! ({enemy_sleep_turns} turns left)")
-                enemy_sleep_turns -= 1
-                turn = True
-                continue
-
-            enemy_attack = random.choice(enemymnst.attacks)
-
-            if enemy_attack.get("type") == "heal":
-                battle_hp_enemy += enemy_attack["base_healing"]
-                print(f"Enemy uses {enemy_attack['name']} and heals {enemy_attack['base_healing']} HP!")
-                print(f"Enemy HP is now {battle_hp_enemy}")
-            else:
-                damage = enemy_attack["base_damage"]
-
-                if enemy_attack.get("element") == "grass" and playermns.type == "water":
-                    damage *= 2
-                    print("It's super effective!")
-                elif enemy_attack.get("element") == "fire" and playermns.type == "grass":
-                    damage *= 2
-                    print("It's super effective!")
-                elif enemy_attack.get("element") == "water" and playermns.type == "fire":
-                    damage *= 2
-                    print("It's super effective!")
-                elif enemy_attack.get("element") == enemymnst.type:
-                    damage /= 2
-                    print("It's not very effective...")
-
-                print(f"Enemy uses {enemy_attack['name']} and deals {damage} damage!")
-                battle_hp_player -= damage
-                print(f"Your HP after enemy attack: {battle_hp_player}")
-
-            turn = True
-            input("\nPress ENTER to continue...")
-
-    if battle_hp_player <= 0:
-        print("You lost the battle!")
-    elif battle_hp_enemy <= 0:
-        print("You won the battle!")
-
-
 class Hert:
     def __init__(self, name, level):
         self.name = name
         self.level = level
         self.HP = 25
+        self.MaxHP = 25
         self.PH_ATTK = 6
         self.EM_ATTK = 3
         self.PH_DEF = 3
@@ -238,7 +128,7 @@ class Maurion:
         self.PH_DEF = 5
         self.EM_DEF = 4
         self.SPD = 7
-        self.type = "fire"
+        self.type = "water"
     def __str__(self):
         return f"{self.name} the {self.__class__.__name__} (Lvl {self.level})"
     attacks = [Bite(), Photosyn(), Tackle(), Spore()]
@@ -273,6 +163,124 @@ class Mirmina:
         return f"{self.name} the {self.__class__.__name__} (Lvl {self.level})"
     attacks = [Bite(), Photosyn(), Tackle(), Spore()]
 
+turn = False
+
+def Battle_loop(playermns, enemymnst, turn, x_accuracy):
+    battle_hp_player = playermns.HP
+    battle_hp_enemy = enemymnst.HP
+    battle_speed_player = playermns.SPD
+    battle_speed_enemy = enemymnst.SPD
+
+    enemy_sleep_turns = 0
+
+    if battle_speed_player > battle_speed_enemy:
+        turn = True
+    elif battle_speed_enemy > battle_speed_player:
+        turn = False
+    else:
+        turn = random.choice([True, False])
+
+    while battle_hp_player > 0 and battle_hp_enemy > 0:
+        if turn:
+            print("\nYour attacks:")
+            for i, attack in enumerate(playermns.attacks):
+                print(f"{i}: {attack['name']}")
+
+            action = input("\nChoose your attack:")
+
+            if action.isdigit():
+                act_choice = int(action)
+                if act_choice < 0 or act_choice >= len(playermns.attacks):
+                    print("Invalid choice, try again.")
+                    continue
+
+                chosen_attack = playermns.attacks[act_choice]
+                print(f"YOU CHOSE ATTACK {chosen_attack['name']}")
+
+                acc_player_attack = chosen_attack.get("accuracy")
+                ran_num = random.randrange(0, 100)
+                if ran_num >= acc_player_attack:
+                    print("you missed")
+                    turn = False
+                    continue
+
+                if chosen_attack.get("type") == "heal":
+                    battle_hp_player += chosen_attack["base_healing"]
+
+                    if battle_hp_player > playermns.MaxHP:
+                        battle_hp_player = playermns.MaxHP
+
+                    print(f"You healed! Your HP is now {battle_hp_player}")
+
+                elif chosen_attack.get("type") == "sleep":
+                    enemy_sleep_turns = random.randint(1, 3)
+                    print(f"The enemy fell asleep for {enemy_sleep_turns} turns!")
+
+                else:
+                    damage = chosen_attack["base_damage"]
+
+                    if chosen_attack.get("element") == "grass" and enemymnst.type == "water":
+                        damage *= 2
+                        print("It's super effective!")
+                    elif chosen_attack.get("element") == "fire" and enemymnst.type == "grass":
+                        damage *= 2
+                        print("It's super effective!")
+                    elif chosen_attack.get("element") == "water" and enemymnst.type == "fire":
+                        damage *= 2
+                        print("It's super effective!")
+                    elif chosen_attack.get("element") == playermns.type:
+                        damage /= 2
+                        print("It's not very effective...")
+
+                    print(f"Enemy HP before attack: {battle_hp_enemy}")
+                    battle_hp_enemy -= damage
+                    print(f"Enemy HP after attack: {battle_hp_enemy}")
+
+            else:
+                print("Please only enter a number")
+            turn = False
+
+
+        else:
+            if enemy_sleep_turns > 0:
+                print(f"The enemy is fast asleep and can't move! ({enemy_sleep_turns} turns left)")
+                enemy_sleep_turns -= 1
+                turn = True
+                continue
+
+            enemy_attack = random.choice(enemymnst.attacks)
+
+            if enemy_attack.get("type") == "heal":
+                battle_hp_enemy += enemy_attack["base_healing"]
+                print(f"Enemy uses {enemy_attack['name']} and heals {enemy_attack['base_healing']} HP!")
+                print(f"Enemy HP is now {battle_hp_enemy}")
+            else:
+                damage = enemy_attack["base_damage"]
+
+                if enemy_attack.get("element") == "grass" and playermns.type == "water":
+                    damage *= 2
+                    print("It's super effective!")
+                elif enemy_attack.get("element") == "fire" and playermns.type == "grass":
+                    damage *= 2
+                    print("It's super effective!")
+                elif enemy_attack.get("element") == "water" and playermns.type == "fire":
+                    damage *= 2
+                    print("It's super effective!")
+                elif enemy_attack.get("element") == enemymnst.type:
+                    damage /= 2
+                    print("It's not very effective...")
+
+                print(f"Enemy uses {enemy_attack['name']} and deals {damage} damage!")
+                battle_hp_player -= damage
+                print(f"Your HP after enemy attack: {battle_hp_player}")
+
+            turn = True
+
+    if battle_hp_player <= 0:
+        print("You lost the battle!")
+    elif battle_hp_enemy <= 0:
+        print("You won the battle!")
+
 def Enemy():
     Monster_dict_number = random.randrange(1, 7)
     enemy = Monsters[Monster_dict_number]
@@ -300,15 +308,14 @@ Monster_choice = ""
 Monster_name = ""
 First_fight = False
 
-for _ in range(1):
-    while Monster_choice == "":
-        openingmsg()
-        Monster_choice = int(input("Please enter the number of the monster you would like te receive.\n: "))
-        Monster_class = Monsters[Monster_choice]
-        Monster_name = input(f"hmmm interesting, you chose {Monster_class.__name__}, huh? Not what I expected. But a good choice nonetheless! What would you like to name your new friend?\n:")
-        playermns = Monster_class(Monster_name, Monster_choice)
-        enemynmr = random.randrange(1, 7)
-        enemy_class = Monsters[enemynmr]
-        enemymnst = enemy_class(f"{enemy_class.__name__}", 2)
-        print(f"Now it’s time to have your first fight! We have chosen a random monster for you to battle. Your opponent will be {enemymnst.name}!")
-    Battle_loop(playermns, enemymnst, True, x)
+while Monster_choice == "":
+    openingmsg()
+    Monster_choice = int(input("Please enter the number of the monster you would like te receive.\n: "))
+    Monster_class = Monsters[Monster_choice]
+    Monster_name = input(f"hmmm interesting, you chose {Monster_class.__name__}, huh? Not what I expected. But a good choice nonetheless! What would you like to name your new friend?\n:")
+    playermns = Monster_class(Monster_name, Monster_choice)
+    enemynmr = random.randrange(1, 7)
+    enemy_class = Monsters[enemynmr]
+    enemymnst = enemy_class(f"{enemy_class.__name__}", 2)
+    print(f"Now it’s time to have your first fight! We have chosen a random monster for you to battle. Your opponent will be {enemymnst.name}!")
+Battle_loop(playermns, enemymnst, True, x)
